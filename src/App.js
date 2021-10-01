4bef059e0336537aa3574a00de38dbbef2b3dde9
omerfarooqBM <omer91999@gmail.com>
2021-10-02 01:25:12 +0500

    aa

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Login from './components/login';
+import Signup from './components/signup';
+function App() {
+  return (
+    <div className="App">
+      {/* <Login /> */}
+      <Signup/>
+    </div>
+  );
+}
+
+export default App;
